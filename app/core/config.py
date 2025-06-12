6f0fd31bb8f0504159baf0ccb25bb027787dac17
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2025-06-12 17:14:09 -0300

    feat: Include other settings on config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,7 @@
 import os
-from typing import Any
+import typing
 
 from alembic.config import Config
-from pydantic import AnyUrl
 from pydantic_settings import BaseSettings
 
 from app.core import BASE_DIR
@@ -11,14 +10,18 @@ ALEMBIC_CFG = Config(os.path.join(BASE_DIR, "alembic.ini"))
 
 
 class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URI: AnyUrl = ALEMBIC_CFG.get_main_option("sqlalchemy.url")
+    SECRET_KEY: typing.Text = None
+
+    SQLALCHEMY_DATABASE_URI: typing.Text = ALEMBIC_CFG.get_main_option("sqlalchemy.url")
+    ALGORITHM: typing.Text = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
         extra = "allow"
         env_file = str(BASE_DIR / ".env")
         env_file_encoding = "utf-8"
 
-    def model_dump(self, *args, **kwargs) -> dict[str, Any]:
+    def model_dump(self, *args, **kwargs) -> dict[str, typing.Any]:
         """Turn .env settings uppercase over pydantic Settings."""
         return {x.upper(): y for x, y in super().model_dump(*args, **kwargs).items()}
 
